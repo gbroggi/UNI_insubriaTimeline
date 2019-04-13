65e4ff07a6d74899bfe8cdc2aacfe11859e3fd2a
gbroggi <giovanni.broggi@gmail.com>
2019-04-13 12:33:05 +0200

    Inizio del progetto timeline con classe Lessons già impostata

---
+++ app/src/main/java/com/example/giova/uni_insubriatimelineapp/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.giova.uni_insubriatimelineapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
